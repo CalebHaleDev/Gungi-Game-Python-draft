58b57220122346f74c4be0c38ee6c0da1cde2526
Caleb Hale <107201448+CalebHaleDev@users.noreply.github.com>
2023-02-28 21:52:21 -0700

    deleted whitespace

--- Gungi.py
+++ Gungi.py
@@ -9,33 +9,23 @@ import math
 
 
 def main():
-
     if ask("Do you want to skip the initial arrangement of the board?"/n) == "no": initialArangement()
     else: board = matrix[9,9,3]
-
-    return board
-    
          
-def GungiGame():
-    print("hello world")
-    
 def initialArangement():
     print("hello world")
-
 
 def move():
     ask("Which piece would you like to move?")
     ask("Where would you like to move the piece?")
     answer = ask("what is the meaning of the universe")
 
-    
 def ask(question):
     while confirmation != "yes":
         pieceSelection = input(question)
         print("You have selected: " + pieceSelect)
         confirmation = input("Continue?")
     return(pieceSelect)
-
 
 def drop():
     print("hello world")
@@ -50,7 +40,5 @@ print("1,2,3,4,5,6,7,8,9" \n
       "1,2,3,4,5,6,7,8,9" \n
       "1,2,3,4,5,6,7,8,9" \n
       "1,2,3,4,5,6,7,8,9")
-
-
 
 
