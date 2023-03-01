7a072b25dec78fde4f700e1fc4e41eecd0088d16
BryceH2000 <126642048+BryceH2000@users.noreply.github.com>
2023-02-28 21:49:19 -0700

    Add files via upload

---
+++ Gungi.py
@@ -0,0 +1,57 @@
+# Gungi Game engine preliminary code
+# 2/28/23
+# This code is just to get ideas and notation written out, final
+#   will be written in Unreal Engine once bugs and visuals
+#   are cleaned up
+
+import math
+
+
+
+def main():
+
+    if ask("Do you want to skip the initial arrangement of the board?"/n) == "no": initialArangement
+
+    else: board = matrix[9,9,3]
+
+    return board
+    
+         
+def GungiGame():
+    print("hello world")
+    
+def initialArangement():
+    print("hello world")
+
+
+def move():
+    ask("Which piece would you like to move?")
+    ask("Where would you like to move the piece?")
+    answer = ask("what is the meaning of the universe")
+
+    
+def ask(question):
+    while confirmation != "yes":
+        pieceSelection = input(question)
+        print("You have selected: " + pieceSelect)
+        confirmation = input("Continue?")
+    return(pieceSelect)
+
+
+def drop():
+    print("hello world")
+
+
+print("1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9" \n
+      "1,2,3,4,5,6,7,8,9")
+
+
+
+
